7f42956a56856fc6a19556a1bc9769cc7422b5d2
Vadim Sirenko <vadim7ko@gmail.com>
2019-10-21 10:02:01 +0300

    Updates

---
+++ precache-manifest.00d83576676182829c62861e035bd7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8261f8c9c81d83ab715133c702afd0",
+    "url": "/trello/index.html"
+  },
+  {
+    "revision": "b0680e65148c0b0bc5ae",
+    "url": "/trello/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "12e0a418ec56fc2c57aa",
+    "url": "/trello/static/js/2.f89a8ad2.chunk.js"
+  },
+  {
+    "revision": "b0680e65148c0b0bc5ae",
+    "url": "/trello/static/js/main.5ca61668.chunk.js"
+  },
+  {
+    "revision": "66ea42950cac1a05e6da",
+    "url": "/trello/static/js/runtime-main.c7fa360c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/trello/static/media/logo.25bf045c.svg"
+  }
+]);
